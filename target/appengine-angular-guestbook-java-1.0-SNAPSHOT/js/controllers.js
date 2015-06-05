66ff4caf4697643f92ea66eb670c5dc1ed552f0f
Gleidson <gleidson.gmoura@gmail.com>
2015-06-05 18:12:23 -0300

    commit

---
+++ target/appengine-angular-guestbook-java-1.0-SNAPSHOT/js/controllers.js
@@ -0,0 +1,40 @@
+'use strict';
+
+gastbook.controller('GuestbookCtrl', function ($scope, $http, $location, $routeParams) {
+
+    $scope.guestbookName = $routeParams['guestbookName'];
+    retrieveGuestbook($routeParams['guestbookName']);
+
+    function retrieveGuestbook(guestbookName) {
+      $http.get('/rest/guestbook/' + encodeURIComponent(guestbookName))
+          .success(function(data) {
+            $scope.greetings = data.greetings;fu0
+            $scope.userServiceInfo = data.userServiceInfo;
+            $scope.guestbookName = data.guestbookName;
+            $scope.currentGuestbookName = data.guestbookName;
+            $location.path(data.guestbookName);
+          })
+          .error(function(data, status) {
+            console.log('Retrieving a guestbook failed with the status code: ' + status);
+            console.log(data);
+          });
+    }
+
+    $scope.submit_form = function () {
+      $http.post(
+          '/rest/guestbook/' + encodeURIComponent($scope.guestbookName),
+          {'content': $scope.content})
+          .success(function (data) {
+            $scope.content = '';
+            $scope.greetings = data.greetings;
+            $scope.guestbookName = data.guestbookName;
+            $scope.currentGuestbookName = data.guestbookName;
+            $location.path(data.guestbookName);
+          })
+          .error(function(data, status) {
+            console.log('Posting a message failed with the status code: ' + status);
+            console.log(data);
+          });
+    };
+  }
+);
---
+++ target/appengine-angular-guestbook-java-1.0-SNAPSHOT/js/filters.js
@@ -0,0 +1,11 @@
+
+'use strict';
+
+/* Filters */
+
+angular.module('guestbook.filters', [])
+.filter('nl2br', function(){
+  return function(text) {
+    return text.replace(/\n/g, '<br>');
+  }
+});
